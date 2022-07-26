672996816b2998b1818578baf2c1fe3371ea5032
Shmuel Hoffman <snyh212@gmail.com>
2022-07-26 13:09:21 -0700

    added table values

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movies (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(100) NOT NULL
+  
+);
+
+CREATE TABLE reviews (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_id INT,
+  review TEXT NOT NULL,
+  FOREIGN KEY (movie_id)
+  REFERENCES movies(id)
+  ON DELETE SET NULL
+);
+
+INSERT INTO movies (movie_name)
+VALUES ("Finding Nemo"),
+       ("Dispicable Me: 1"),
+       ("Minions: Yellow is the new Black"),
+       ("Dispicable Me: 2"),
+       ("Minions: Rise of Gru");
+
+INSERT INTO reviews (review)
+VALUES ("2"),
+       ("4"),
+       ("4"),
+       ("4"),
+       ("5");
